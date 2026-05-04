01ac72f5e0b4071745ab0d9a4ef5cde08aaf058b
raulesteban4 <raul.esteban4@educa.madrid.org>
2026-05-04 18:38:25 +0200

    miapp duplicado

--- mi-app/modelos/Juego.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const juegoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  genero: String,
-  year: Number
-});
-
-module.exports = mongoose.model('Juego', juegoSchema);
--- mi-app/utils/authMiddleware3.js
+++
@@ -1,9 +0,0 @@
-const authMiddleware = (req, res, next) => {
-  if (!req.session.usuarioId) {
-    return res.status(401).json({ message: 'No autenticado. Por favor, haz login.' });
-  }
-  req.usuarioId = req.session.usuarioId;
-  next();
-};
-
-module.exports = authMiddleware;
